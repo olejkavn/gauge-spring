00ad2068a50aba971cf013d330cc36bbb3cde3e3
otymkovskyi <otymkovskyi@playtika.com>
2020-03-25 20:01:21 +0200

    Initial commit

---
+++ common/src/main/java/com/thoughtworks/gauge/example/AnotherTestSteps.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.gauge.example;
+
+import com.thoughtworks.gauge.Step;
+
+public class AnotherTestSteps {
+
+    @Step("Demo step")
+    public void someStep() {
+        assert false;
+    }
+}
